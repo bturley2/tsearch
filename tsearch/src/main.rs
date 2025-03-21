c6eddc4c538ed550ee109ec9b0f9a914465811bc
Bryce Turley <turleyb2@gmail.com>
2025-03-20 18:08:22 -0700

    app now runs

--- tsearch/src/main.rs
+++ tsearch/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{
+    routing::get,
+    Router
+};
+
+#[tokio::main]
+async fn main() {
+    // build app and add single route
+    let app = Router::new().route("/", get(|| async { "Hello World!"}));
+
+    // run the app with hyper
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:9999").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
